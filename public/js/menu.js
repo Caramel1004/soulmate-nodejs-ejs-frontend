bd2a9c43a2f53470bddf71cc3bc858e5422da2cf
Caramel1004 <Caramel1004.js@gmail.com>
2023-07-16 08:08:22 +0900

    화면 애니메이션 추가

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,9 +1,9 @@
 const activeMenuBtnColor = () => {
     const url = window.location.href;
-    const replaceUrl = url.replace('?','/');
+    const replaceUrl = url.replace('?', '/');
     const path = replaceUrl.split('/')[3];
 
-    if(!path) {
+    if (!path) {
         document.getElementById('/').style.background = 'rgb(219, 219, 232)';
     }
 
@@ -11,4 +11,21 @@ const activeMenuBtnColor = () => {
     document.getElementById(path).style.background = 'rgb(219, 219, 232)';
 }
 
+const toggleBtn = id => {
+    const itemBoxStyle = document.getElementById(id).style;
+
+    if (itemBoxStyle.display === 'none') {
+        itemBoxStyle.display = 'block';
+        itemBoxStyle.animation = '3s infinite alternate slidein';
+    } else {
+        itemBoxStyle.display = 'none';
+    }
+}
+
 window.addEventListener('DOMContentLoaded', activeMenuBtnColor);
+document.getElementById('chat').addEventListener('click', () => {
+    toggleBtn('chat-list');
+});
+document.getElementById('workspace').addEventListener('click', () => {
+    toggleBtn('workspace-list');
+});
