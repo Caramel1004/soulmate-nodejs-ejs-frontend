d6fdd3b7f2a1308e1262b0ea5f6860a9667ff293
Caramel1004 <Caramel1004.js@gmail.com>
2023-10-18 23:05:15 +0900

    회원가입 API추가

--- public/js/auth.js
+++ public/js/auth.js
@@ -42,6 +42,10 @@ const onClickPhotoModalBtn = () => {
     modal.querySelector('label[for="file"]').addEventListener('change', onChangeSelectFile);
 }
 
+const onClickSubmitBodyBtn = async () => {
+    await postSignUp();
+}
+
 const createModalTagtoSelectUserProfilePhoto = () => {
     const base64EncodedFile = document.getElementById('photo-file').dataset.src || 'https://d2u3dcdbebyaiu.cloudfront.net/uploads/atch_img/309/59932b0eb046f9fa3e063b8875032edd_crop.jpeg';
     const fileName = document.getElementById('filename').textContent;
@@ -67,12 +71,15 @@ const createModalTagtoSelectUserProfilePhoto = () => {
         </div>
         <div class="box__button__submit">
             <button type="submit" id="complete">완료</button>
-            <a id="cancel">취소</a>
+            <a id="modal-close">취소</a>
         </div>`;
-    console.log(modal);
+
     // document.getElementById('file').files = thumbnailFiles;
     modalBackGround.appendChild(modal);
     document.body.appendChild(modalBackGround);
+    document.getElementById('modal-close').addEventListener('click', () => {
+        document.body.removeChild(document.querySelector('.modal-background'));
+    })
     return modal;
 }
 
@@ -101,7 +108,7 @@ const changePreviewPhoto = e => {
         const imgTag = document.querySelector('.modal-add-mode').querySelector('img');
         console.log(imgTag);
         imgTag.src = base64EncodedFile;
-        document.querySelector('.create-channel-photo-input-box').querySelector('label[for="file"]').insertAdjacentText('afterbegin',document.getElementById('file').files[0].name);
+        document.querySelector('.create-channel-photo-input-box').querySelector('label[for="file"]').insertAdjacentText('afterbegin', document.getElementById('file').files[0].name);
     } catch (error) {
         alert(error)
         console.log(error)
@@ -119,7 +126,7 @@ const onClickCompleteBtn = className => {
     console.log(base64EncodedFile)
     document.getElementById('photo-file').textContent = photo.name;
     document.getElementById('photo-file').files = photo;
-    document.getElementById('photo-file').setAttribute('data-src',base64EncodedFile);
+    document.getElementById('photo-file').setAttribute('data-src', base64EncodedFile);
     document.getElementById('filename').textContent = photo[0].name;
     document.getElementById('filename').style.color = '#1d1c1d';
     removeChildrenTag(className);
@@ -131,11 +138,80 @@ const removeChildrenTag = className => {
     document.body.removeChild(parentNode);
 }
 
+const insertVaildationErrorMsg = async errorObj => {
+    try {
+        const inputForm = document.querySelector('.signup-form__box');
+        const inputFormChildren = inputForm.querySelectorAll('.input-box');
+
+        if (document.getElementById('valid')) {
+            document.getElementById('valid').parentNode.removeChild(document.getElementById('valid'));
+            document.getElementById('valid-box').parentNode.removeChild(document.getElementById('valid-box'));
+        }
+        if (errorObj.path === 'gender') {
+            document.getElementById('gender').insertAdjacentHTML('beforeend', `<div id="valid-box"><span class="valid" id="valid">*${errorObj.msg}</span></div>`);
+            return;
+        }
+        for (const inputBox of inputFormChildren) {
+            const matchedTag = inputBox.querySelector(`input[name="${errorObj.path}"]`);
+ 
+            if (matchedTag) {
+                inputBox.insertAdjacentHTML('beforeend', `<div id="valid-box"><span class="valid" id="valid">*${errorObj.msg || errorObj.message}</span></div>`);
+                break;
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const postSignUp = async () => {
+    const email = document.querySelector('.signup-form__box').querySelector('input[name="email"]');
+    const password = document.querySelector('.signup-form__box').querySelector('input[name="password"]');
+    const name = document.querySelector('.signup-form__box').querySelector('input[name="name"]');
+    const photo = document.querySelector('.signup-form__box').querySelector('input[type="file"], input[name="photo"]');
+    let gender = '';
+    const openRadioTags = document.getElementById('gender').querySelectorAll('input[type="radio"], input[name="gender"]');
+    for (const radio of openRadioTags) {
+        if (radio.checked) {
+            gender = radio.value;
+        }
+    }
+    const phone = document.querySelector('.signup-form__box').querySelector('input[name="phone"]');
+    const comment = document.querySelector('.signup-form__box').querySelector('textarea[name="comment"]');
+    try {
+        const formData = new FormData();
+        formData.append('email', email.value);
+        formData.append('emailFront', email.value.split('@')[0]);
+        formData.append('password', password.value);
+        formData.append('name', name.value);
+        formData.append('gender', gender);
+        formData.append('photo', photo.files[0]);
+        formData.append('phone', phone.value);
+        formData.append('comment', comment.value);
+        const response = await fetch(`http://localhost:3000/signup`, {
+            method: 'POST',
+            body: formData
+        })
+
+        const data = await response.json();
+        console.log(data);
+        if (data.error) {
+            insertVaildationErrorMsg(data.error);
+            return;
+        } else {
+            return window.location.href = `/login`;
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 document.querySelector('.open-sign-form').addEventListener('click', onClickOpenSignupForm);
 document.getElementById('cancel').addEventListener('click', onClickOpenLoginForm);
 document.querySelector('label[for="male"]').addEventListener('click', onClickOpenYNRadio);
 document.querySelector('label[for="female"]').addEventListener('click', onClickOpenYNRadio);
 document.querySelector('label[for="user-profile-photo"]').addEventListener('click', onClickPhotoModalBtn);
+document.getElementById('submit').addEventListener('click', onClickSubmitBodyBtn);
 // document.getElementById('user-profile-photo').addEventListener('click', onClickPhotoModalBtn);
 // 구글 버튼
 // const googleUser = {};
