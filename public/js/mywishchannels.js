a27534d818ca64f0a433190e4cc06b7a19fb3801
Caramel1004 <Caramel1004.js@gmail.com>
2023-11-07 23:13:00 +0900

    검색 박스에 keydown 이벤트 추가

--- public/js/mywishchannels.js
+++ public/js/mywishchannels.js
@@ -7,6 +7,17 @@ const onClickChannelBox = async channelId => {
     window.location.href = `http://localhost:3000/open/${channelId}`;
 }
 
+const onKeyDownSearchBox = async e => {
+    if (e.keyCode === 13 && e.target.value !== '') {
+        try {
+            console.log('엔터키 누름!!');
+            await getSearchWishChannelsByKeyWord(e);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
+
 /** ----------------- 태그관련 함수 ----------------- */
 const removeWishChannelTag = channelId => {
     const removeTag = document.getElementById(channelId).parentNode.parentNode;
@@ -19,6 +30,23 @@ const removeWishChannelTag = channelId => {
     }
 }
 
+const activeSearchTypeBtnColor = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const category = urlParams.get('category');
+    console.log(category);
+    const tabBox = document.getElementById('tab-box');
+    const tabBtns = tabBox.querySelectorAll('a');
+
+    for(const btn of tabBtns) {
+        const href = new URL(btn.href);
+        const query = href.searchParams.get('category');
+        if(query === category) {
+            btn.style.color = '#ffffff';
+            btn.style.background = '#000000';
+            break;
+        }
+    }
+}
 
 /** ----------------- API 요청 함수 -----------------*/
 const postAddOpenChannelToWishChannel = async channelId => {
@@ -41,7 +69,30 @@ const postAddOpenChannelToWishChannel = async channelId => {
     }
 }
 
+const getSearchWishChannelsByKeyWord = async e => {
+    const categoryTag = document.getElementById('box-search').querySelector('select[name="category"], select[id="category"]');
+    const category = categoryTag.options[categoryTag.selectedIndex].value;
+    const searchWord = e.target.value;
+    console.log(category)
+    console.log(searchWord);
+    let URL;
+    if (searchWord !== '') {
+        URL = `http://localhost:3000/wishchannels?category=${category}&searchWord=${searchWord}`;
+    } else {
+        URL = `http://localhost:3000`;
+    }
+    try {
+        window.location.href = URL;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 /** ----------------- 이벤트 리스너 -----------------*/
+window.addEventListener('DOMContentLoaded', activeSearchTypeBtnColor);
 document.getElementById('thumbnail').addEventListener('mouseover', () => {
     document.getElementById('thumbnail').style.cursor = 'pointer';
+})
+document.getElementById('search').addEventListener('keydown', e => {
+    onKeyDownSearchBox(e);
 })
