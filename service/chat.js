ec31e49d747927fe3487405c29362e1db6b3bbec
Caramel1004 <Caramel1004.js@gmail.com>
2023-11-11 23:41:33 +0900

    채팅로그 날짜별로 정렬, 채팅방 파일함 날짜별로 정렬

--- service/chat.js
+++ service/chat.js
@@ -6,46 +6,60 @@ const chatService = {
         try {
             const data = await chatAPI.getLoadChatRoom(jsonWebToken, refreshToken, channelId, chatRoomId, next);
 
-            const chatObjList = data.chatRoom.chats;
-
+            let chatObjList = data.chatRoom.chats;
+            const chatSetList = [];
             if (chatObjList.length > 0) {
+                let tmp = chatObjList;
+                while (tmp.length > 0) {
+                    let startDate = new Date(tmp[0].createdAt).toLocaleDateString('ko', { year: 'numeric', month: 'long', day: 'numeric' });
+                    const sameDateList = chatObjList.filter(chat => new Date(chat.createdAt).toLocaleDateString('ko', { year: 'numeric', month: 'long', day: 'numeric' }) == startDate);
+                    console.log('sameDateList: ', sameDateList);
+                    chatSetList.push(sameDateList);
+                    tmp = [...tmp.filter(chat => new Date(chat.createdAt).toLocaleDateString('ko', { year: 'numeric', month: 'long', day: 'numeric' }) != startDate)];
+                    console.log('tmp: ', tmp);
+                }
+                console.log('chatSetList: ', chatSetList);
+            }
+
+            if (chatSetList.length > 0) {
                 let year;
                 let month;
                 let day;
-                chatObjList.map(chat => {
-                    year = new Date(chat.createdAt).getFullYear();
-                    month = new Date(chat.createdAt).getMonth() + 1;
-                    day = new Date(chat.createdAt).getDate();
-                    const timestamp = new Date(chat.createdAt).toTimeString().split(' ')[0];//ex)09:51:35 GMT+0900 (한국 표준시)
+                for (const set of chatSetList) {
+                    set.map(chat => {
+                        year = new Date(chat.createdAt).getFullYear();
+                        month = new Date(chat.createdAt).getMonth() + 1;
+                        day = new Date(chat.createdAt).getDate();
+                        const timestamp = new Date(chat.createdAt).toTimeString().split(' ')[0];//ex)09:51:35 GMT+0900 (한국 표준시)
 
-                    let hour = parseInt(timestamp.split(':')[0]);
+                        let hour = parseInt(timestamp.split(':')[0]);
 
-                    const when = hour >= 12 ? '오후' : '오전';
+                        const when = hour >= 12 ? '오후' : '오전';
 
-                    if (month < 10) {
-                        month = '0' + month;
-                    } else if (month > 12) {
-                        month = '0' + 1;
-                    }
+                        if (month < 10) {
+                            month = '0' + month;
+                        } else if (month > 12) {
+                            month = '0' + 1;
+                        }
 
-                    if (day < 10) {
-                        day = '0' + day
-                    }
-                    if (when === '오후' && hour > 12) {
-                        hour %= 12;
-                    }
+                        if (day < 10) {
+                            day = '0' + day
+                        }
+                        if (when === '오후' && hour > 12) {
+                            hour %= 12;
+                        }
 
-                    const min = timestamp.split(':')[1];
+                        const min = timestamp.split(':')[1];
 
-                    chat.fomatDate = `${year}-${month}-${day}  ${when} ${hour}:${min}`;
+                        chat.fomatDate = `${year}년 ${month}월 ${day}일`;
+                        chat.fomatTime = `${when} ${hour}:${min}`;
 
-                    return chat;
-                });
-
-                // console.log(chatObjList)
-                // console.log('data: ', data.chatRoom.chats[0]);
-                // data.chatRoomData.chats = [...chatObjList];
+                        return chat;
+                    });
+                }
             }
+            data.chatRoom.chats = [...chatSetList];
+            console.log('data.chatRoom.chats: ', data.chatRoom.chats);
             return data;
         } catch (err) {
             next(err);
@@ -84,7 +98,34 @@ const chatService = {
     getLoadFilesInChatRoom: async (token, refreshToken, channelId, chatRoomId, next) => {
         try {
             const data = await chatAPI.getLoadFilesInChatRoom(token, refreshToken, channelId, chatRoomId, next);
+            let chatObjList = data.chatsWithFileUrlsInChatRoom;
+            const chatSetList = [];
+            if (chatObjList.length > 0) {
+                let tmp = chatObjList;
+                while (tmp.length > 0) {
+                    let startDate = new Date(tmp[0].createdAt).toLocaleDateString('ko', { year: 'numeric', month: 'long', day: 'numeric' });
+                    const sameDateList = chatObjList.filter(chat => new Date(chat.createdAt).toLocaleDateString('ko', { year: 'numeric', month: 'long', day: 'numeric' }) == startDate);
+                    console.log('sameDateList: ', sameDateList);
+                    chatSetList.push(sameDateList);
+                    tmp = [...tmp.filter(chat => new Date(chat.createdAt).toLocaleDateString('ko', { year: 'numeric', month: 'long', day: 'numeric' }) != startDate)];
+                }
+            }
 
+            if (chatSetList.length > 0) {
+                for (const set of chatSetList) {
+                    set.map(chat => {
+                        chat.createdAt = new Date(chat.createdAt).toLocaleDateString('ko', { 
+                            year: 'numeric', 
+                            month: 'long', 
+                            day: 'numeric',
+                            weekday: 'long'
+                        });
+                        return chat;
+                    });
+                }
+            }
+
+            data.chatsWithFileUrlsInChatRoom = [...chatSetList];
             return data;
         } catch (err) {
             next(err);
