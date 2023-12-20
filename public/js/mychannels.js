92bf8aa21fd40a2da0eb4946ba7225d618fce05a
Caramel1004 <Caramel1004.js@gmail.com>
2023-12-20 23:31:55 +0900

    검색박스 키다운 이벤트 함수 추가, 페이지 로딩 후 버튼, 카테고리박스, 검색박스에 이전 페이지에서 사용한 정보 세팅

--- public/js/mychannels.js
+++ public/js/mychannels.js
@@ -1,21 +1,59 @@
-const activeSearchWordBtnColor = () => {
+const onKeyDownSearchBox = async e => {
+    if (e.keyCode === 13 && e.target.value !== '') {
+        try {
+            console.log('엔터키 누름!!');
+            await getSearchUserOfChannelsByKeyWord(e);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
+
+const setSearchWordInSearchBoxAndCategoryInSelectBox = () => {
     const urlParams = new URLSearchParams(window.location.search);
     const searchWord = urlParams.get('searchWord');
+    const category = urlParams.get('category');
     
-    console.log('searchWord: ', searchWord);
-    document.getElementById(searchWord).style.color = '#ffffff';
-    document.getElementById(searchWord).style.background = 'black';
+    // 검색 키워드
+    document.getElementById('search').value = searchWord;
+    
+    //카테고리 체크 => select 박스는 프로퍼티 selected
+    const categoryTagOptions = document.querySelector('select[name="category"], select[id="category"]').options;
+    const categoryTag = Array.prototype.slice.call(categoryTagOptions).find(target => target.value == category);
+    categoryTag.selected = true
+    categoryTag.options[categoryTag.selectedIndex].value = category;
 }
 
-const activeSearchTypeBtnColor = () => {
+const getSearchUserOfChannelsByKeyWord = async e => {
+    const categoryTag = document.getElementById('box-search').querySelector('select[name="category"], select[id="category"]');
+    let category = categoryTag.options[categoryTag.selectedIndex].value;
+    const searchWord = e.target.value;
     const urlParams = new URLSearchParams(window.location.search);
     const searchType = urlParams.get('searchType');
-    
-    console.log('searchType: ', searchType);
-    document.getElementById(searchType).style.color = '#ffffff';
-    document.getElementById(searchType).style.background = 'black';
-    document.getElementById(searchType).querySelector('i').style.color = '#ffffff';
+
+    if(category == '전체') {
+        category = '';
+    }
+
+    if(searchWord == undefined) {
+        searchWord = '';
+    }
+    let URL;
+    if (searchWord !== '' && category !== '') {
+        URL = `http://localhost:3000/mychannels?searchType=${searchType}&category=${category}&searchWord=${searchWord}`;
+    } else if(category == '' && searchWord == ''){
+        URL = `http://localhost:3000/mychannels?searchType=${searchType}`;
+    }else if(category == '' && searchWord !== '') {
+        URL = `http://localhost:3000/mychannels?searchType=${searchType}&searchWord=${searchWord}`;
+    }
+    try {
+        window.location.href = URL;
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-window.addEventListener('DOMContentLoaded', activeSearchWordBtnColor);
-window.addEventListener('DOMContentLoaded', activeSearchTypeBtnColor);
+window.addEventListener('DOMContentLoaded', setSearchWordInSearchBoxAndCategoryInSelectBox);
+document.getElementById('search').addEventListener('keydown', e => {
+    onKeyDownSearchBox(e);
+})
